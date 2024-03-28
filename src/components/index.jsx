1b9de4126cb41a4a5baae062c47e0e586155c4c5
Kataomoi <114056087+kataomoidayo@users.noreply.github.com>
2024-03-28 22:10:57 +0800

    update content

--- src/components/index.jsx
+++ src/components/index.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { createRoot } from 'react-dom/client';
-import NotesApp from './NotesApp';
+import NotesApp from './notes/NotesApp';
 
 import '../styles/style.css';
 
